d8d7b663850bfa20500ac63e5ebb4d48b9f6d5d7
Roman Khomenko <dreamerixx@gmail.com>
2016-10-05 17:42:51 +0600

    Updated js

--- public/js/map.js
+++ public/js/map.js
@@ -7,6 +7,9 @@ DG.then(function() {
     //Add popup
     // DG.marker([54.98, 73.37]).addTo(map).bindPopup('Я попап!');
 
+
+    var myIcons = [];
+    var colors = ['']
     myIcons[0] = DG.divIcon({
         html: '<div style="background-color: #c8a2c8; width: 8px; height: 8px;"></div>',
         iconSize: [8, 8]
@@ -34,17 +37,10 @@ DG.then(function() {
     $.ajax({
       url: 'http://catalog.api.2gis.ru/search?what=%D1%80%D0%B0%D0%B7%D0%BB%D0%B8%D0%B2%D0%BD%D0%BE%D0%B5%20%D0%BF%D0%B8%D0%B2%D0%BE&where=%D0%9E%D0%BC%D1%81%D0%BA&version=1.3&format=short&key=ruczoy1743',
       success: function(data) {
-
-        var myIcons = [];
-        var colors = ['']
-
-
-
-
         $.each(data.result, function(index, value ) {
             console.log(index);
             // console.log(value);
-            indexIcon = 5;
+            var indexIcon = 5;
             if (index < 3) {
                 indexIcon = index;
             }
@@ -56,7 +52,20 @@ DG.then(function() {
     });
 
 
-    map.on('click', function(ev) {
-        console.log(ev.latlng);
+    map.on('click', function(e) {
+        console.log(e.latlng);
+        $('input[name="lat"]').val(e.latlng.lat);
+        $('input[name="lon"]').val(e.latlng.lng);
+    });
+
+    $('#saveMarker').on('click', function(e) {
+        var listCustomMarkers = $('#listCustomMarkers');
+        var row = '';
+        var color = ($('input[name="color"]:checked').val() == 'red') ? 'danger' : 'success';
+        row += '<li class="list-group-item list-group-item-' + color + '">';
+        row += $('input[name="lat"]').val() + ' ' + $('input[name="lon"]').val();
+        row += '</li>';
+
+        listCustomMarkers.append(row);
     });
 });
--- resources/views/map/index.blade.php
+++ resources/views/map/index.blade.php
@@ -6,9 +6,48 @@
         <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
     </head>
     <body>
-        <div id="map" style="width: 100%; height: 512px;"></div>
-        <button class="pull-right">sds</button>
-        
+        <div class="container form-horizontal">
+            <h1>2gis Markers</h1>
+            <div class="col-sm-3">
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">Lat:</label>
+                    <div class="col-sm-10">
+                        <input type="number" name="lat" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">Lon:</label>
+                    <div class="col-sm-10">
+                        <input type="number" name="lon" class="form-control">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">Text:</label>
+                    <div class="col-sm-10">
+                        <textarea class="form-control" name="text"></textarea>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">Color:</label>
+                    <div class="col-sm-10 radio">
+                        <label><input type="radio" name="color" value="green" checked>Green</label>
+                        <label><input type="radio" name="color" value="red">Red</label>
+                    </div>
+                </div>
+
+                <button id="saveMarker" class="btn btn-default">Save</button>
+                <hr>
+
+                <ul id="listCustomMarkers" class="list-group"></ul>
+            </div>
+
+            <div class="form-group col-sm-9">
+                <div id="map" style="width: 100%; height: 768px;"></div>
+            </div>
+        </div>
+
         <script src="http://maps.api.2gis.ru/2.0/loader.js"></script>
         <script src="http://code.jquery.com/jquery-3.1.1.min.js"></script>
         <script src="/js/map.js"></script>
